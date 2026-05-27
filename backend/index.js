98cfba37570aa7d9cf66fa240c0ed3bd269b08ad
jvlian <julianvikbusiness@gmail.com>
2026-05-27 12:37:31 +0700

    0.0.0-4

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,26 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.route.js"
 
-dotenv.config();
+dotenv.config()
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("Database connected!");
+    console.log("Database is connected")
   })
   .catch((err) => {
-    console.log(err);
-  });
+    console.log(err)
+  })
 
-const app = express();
+const app = express()
+
+// for allowing json object in req body
+app.use(express.json())
 
 app.listen(5000, () => {
-  console.log("Running in port 5000!");
-});
+  console.log("Server is running on port 5000!")
+})
+
+app.use("/api/auth", authRoutes)
