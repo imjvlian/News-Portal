5f2d5ee72da3430386d2761913f79467bb591551
jvlian <julianvikbusiness@gmail.com>
2026-05-27 13:05:23 +0700

    0.0.0-5

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,9 @@
-import bcryptjs from "bcryptjs"
-import User from "../models/user.model.js"
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
-  const { username, email, password } = req.body
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
 
   if (
     !username ||
@@ -12,22 +13,22 @@ export const signup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    return res.status(400).json({ message: "All fields are required" })
+    return next(errorHandler(400, "All field are required!"));
   }
 
-  const hashedPassword = bcryptjs.hashSync(password, 10)
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
   const newUser = new User({
     username,
     email,
     password: hashedPassword,
-  })
+  });
 
   try {
-    await newUser.save()
+    await newUser.save();
 
-    res.json("Signup successful")
+    res.json("Signup successful");
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    next(error);
   }
-}
+};
