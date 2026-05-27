9f5da8eabded16b6bf167dc99e872694574305a4
jvlian <julianvikbusiness@gmail.com>
2026-05-28 00:39:18 +0700

    0.0.0-6

---
+++ frontend/src/components/ui/label.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cva } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const labelVariants = cva(
+  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+);
+
+const Label = React.forwardRef(({ clasName, ...props }, ref) => (
+  <LabelPrimitive.Root
+    ref={ref}
+    clasName={cn(labelVariants(), clasName)}
+    {...props}
+  />
+));
+
+Label.displayName = LabelPrimitive.Root.displayName;
+
+export { Label };
