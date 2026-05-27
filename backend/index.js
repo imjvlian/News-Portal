c52da43226e2edb2b91b97f9f08c91659237b8ce
jvlian <julianvikbusiness@gmail.com>
2026-05-27 11:27:03 +0700

    0.0.0-3

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
 
-app.listen(5000, () =>{
-    console.log("Running in port 5000!")
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.listen(5000, () => {
+  console.log("Running in port 5000!");
+});
